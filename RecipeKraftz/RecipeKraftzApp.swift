d4317aa414bf8b649df80baec61b8df0bdd9fe05
CWKrahtz <221198@virtualwindow.co.za>
2023-10-15 12:20:23 +0200

    Initial Commit

---
+++ RecipeKraftz/RecipeKraftzApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipeKraftzApp.swift
+//  RecipeKraftz
+//
+//  Created by student on 2023/10/15.
+//
+
+import SwiftUI
+
+@main
+struct RecipeKraftzApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
