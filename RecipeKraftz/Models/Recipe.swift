904eb4e02c43c4ec67c2aa970712cc52041e53b1
CWKrahtz <221198@virtualwindow.co.za>
2023-10-18 16:00:34 +0200

    Update Recipe Swift

--- RecipeKraftz/Models/Recipe.swift
+++ RecipeKraftz/Models/Recipe.swift
@@ -17,4 +17,9 @@ struct Recipe: Identifiable{
     var img: String
 }
 
-let dummyRecipe: Recipe = Recipe(name: "Crème Brulee", author: "Rene Du Toit", description: "It is a classic French dessert that consists of a rich and creamy custard base, typically flavored with vanilla, and a crisp caramelized sugar crust on top.", ingredients: "1 Liter Cream. 120 gram White Chocolate. 200 gram Cater Suger. 13 Egg Yolks. White/Brown Suger (own preference)", steps: "Melt chocolate, sugar, and cream together. When blended let cool to room temperature then mix in egg yolks. Strain mixture. Pour into cups/rumekins. Place in bakiing dish with boiling water. Cover with foil. Bake at 150ºC for 40-45 min. Let it col down. Put in fridge overnight or until you are happy with consitency. Pour over desired amout of suger and carmelise suger till satisfied", img: "Creme-Brulee")
+let dummyRecipe: Recipe = Recipe(
+    name: "Crème Brulee",
+    author: "Rene Du Toit",
+    description: "It is a classic French dessert that consists of a rich and creamy custard base, typically flavored with vanilla, and a crisp caramelized sugar crust on top.",
+    ingredients: "1 Liter Cream. 120 gram White Chocolate. 200 gram Cater Suger. 13 Egg Yolks. White/Brown Suger (own preference)",
+    steps: "Melt chocolate, sugar, and cream together. When blended let cool to room temperature then mix in egg yolks. Strain mixture. Pour into cups/rumekins. Place in bakiing dish with boiling water. Cover with foil. Bake at 150ºC for 40-45 min. Let it col down. Put in fridge overnight or until you are happy with consitency. Pour over desired amout of suger and carmelise suger till satisfied", img: "Creme-Brulee")
